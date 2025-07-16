69be9278fc551153a04a9d14cabf14d18473bd3d
Abdallah-BinSaif <binsaif.info@gmail.com>
2025-07-16 12:39:13 +0600

    restructure the code of finding the single todoItem and completed button working

--- index.js
+++ index.js
@@ -6,17 +6,35 @@ addTodoInput.addEventListener("submit", (event)=>{
     const todo = document.getElementById("ToDo").value
     const singleTodo = createSingleTodo(todo)
     todoContainer.appendChild(singleTodo)
+    document.getElementById("ToDo").value = ""
 })
 
 todoContainer.addEventListener("click", function(event){
+    /* change
+
     const id =event.target.parentElement.parentElement.getAttribute("id") || ""
     const todoItem = document.getElementById(id)
-    const todoButtons = todoItem?.getElementsByTagName("button") || ""
-    const action = event.target.getAttribute("class")
+
+    changing this two line of Code into a while loop for finding todoItem with a better approach
+    */
+    let currentElement = event.target;
+    let todoItem = null;
+    while (currentElement && currentElement !== todoContainer) {
+        if (currentElement.classList.contains("single-todo")) {
+            todoItem = currentElement
+            break;
+        }
+        currentElement = currentElement.parentElement
+    }
+    /* change
+    const todoButtons = todoItem.getElementsByTagName("button") || ""
+    const checkButton = todoItem.querySelector(".single-todo .check-button")
+    
     if(action === "todo") {
         todoItem.style.backgroundColor = "green"
-        todoButtons[1]?.classList.remove("todo")
-        todoButtons[1]?.classList.add("done")
+        checkButton.classList.remove("todo")
+        checkButton.classList.add("done")
+        checkButton.innerHTML = "&leftarrow;"
     }else if(action === "done") {
         todoItem.style.backgroundColor = "azure"
         todoButtons[1]?.classList.remove("done")
@@ -24,7 +42,26 @@ todoContainer.addEventListener("click", function(event){
     }else if(action === "delete") {
         todoContainer.removeChild(todoItem)
     }
-    console.log(todoButtons, "action", action)
+
+    changing this code for more control over the single todo list
+    */
+   const action = event.target.classList[0]
+   if(action === "todo" || action === "done") {
+    todoItem.classList.toggle("completed")
+    const checkButton = todoItem.querySelector(".check-button");
+    if(todoItem.classList.contains("completed")) {
+        checkButton.classList.remove("todo", "check-button")
+        checkButton.classList.add("done", "check-button")
+        checkButton.innerHTML = "&leftarrow;"
+    }else{
+        checkButton.classList.remove("done", "check-button")
+        checkButton.classList.add("todo", "check-button")
+        checkButton.innerHTML = "&check;"
+    } 
+
+   }else if(action === "delete") {
+        todoContainer.removeChild(todoItem)
+    }
 })
 
 function createSingleTodo(todo) {
@@ -43,10 +80,10 @@ function actionSpan() {
     const actionSpan = document.createElement("span")
     const deleteButton = document.createElement("button")
     deleteButton.innerHTML = "&cross;"
-    deleteButton.classList.add("delete")
+    deleteButton.classList.add("delete", "delete-button")
     const checkButton = document.createElement("button")
     checkButton.innerHTML = "&check;"
-    checkButton.classList.add("todo")
+    checkButton.classList.add("todo", "check-button")
     actionSpan.appendChild(deleteButton)
     actionSpan.appendChild(checkButton)
     return actionSpan
